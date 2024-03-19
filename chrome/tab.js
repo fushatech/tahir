642f5aa38df6332422f386fac92e689ecb4ec2ea
touhid <udoy.touhid@gmail.com>
2024-03-20 03:07:53 +0600

    Add state variable

--- chrome/tab.js
+++ chrome/tab.js
@@ -56,6 +56,9 @@ function isDomainIgnored() {
 }
 
 
+var lastTargetElement = null;
+var lastTargetElementCss = null;
+
 /* addListeners - (1) adds message listeners to receive specific messages from popup.js (popup modal) & background.js (key commands) & (2) routes to appropriate functions on receipt */
 function addListeners () {
 	chrome.runtime.onMessage.addListener(
